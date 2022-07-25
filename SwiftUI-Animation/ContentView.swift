0175d1cf74ac065ecd8977b0ad9d7a05d436e324
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-25 10:47:39 -0700

    Initial Commit

---
+++ SwiftUI-Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Animation
+//
+//  Created by Mitya Kim on 7/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI-Animation
+//
+//  Created by Mitya Kim on 7/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
